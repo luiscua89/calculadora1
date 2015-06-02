4546938d83a60d6937642f58c9963cb2cae86e47
Luis Cua Catzin <luiscuacatzin@Luiss-Mac.local>
2015-06-02 14:05:01 -0700

    commit

--- Calculadora1/ViewController.swift
+++ Calculadora1/ViewController.swift
@@ -9,7 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //---------Decalaraciones /----------
+    @IBOutlet weak var screen: UILabel!
+    
+    var firstNumber = Int()
+    var secondNumber = Int()
+    var isTypingNumber = false
+    var result = Int()
+    var operation = ""
+    
+    @IBAction func number(sender: AnyObject) {
+        var number = sender.currentTitle
+        if isTypingNumber == true {
+            screen.text = screen.text! + number!!
+        } else {
+            screen.text = number
+        }
+        isTypingNumber = true
+        
+    }
+    
+    @IBAction func operation(sender: AnyObject) {
+        isTypingNumber = false
+        firstNumber = screen.text!.toInt()!
+        operation = sender.currentTitle!!
+        
+    }
 
+    @IBAction func equals(sender: AnyObject) {
+        secondNumber = screen.text!.toInt()!
+        if operation == "+" {
+            result = firstNumber + secondNumber
+        }
+        
+        else if operation == "-" {
+            result = firstNumber - secondNumber
+        }
+        
+        else if operation == "÷" {
+            result = firstNumber / secondNumber
+        }
+        
+        else {
+            result = firstNumber * secondNumber
+        }
+        
+        screen.text = "\(result)"
+        
+    }
+    
+    @IBAction func clear(sender: AnyObject) {
+        firstNumber = 0
+        secondNumber = 0
+        result = 0
+        isTypingNumber = false
+        
+        screen.text = "\(result)"
+    }
+    
+    
+    //------------------------------------------
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
